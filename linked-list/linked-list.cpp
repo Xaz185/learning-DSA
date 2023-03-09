0955f1257a9f2d04a9e355f4ba4e477863d64ecc
Xavier <xavier.jenni@hotmail.com>
2023-03-09 15:02:55 +0100

    Added the strcutures of an Element and of the linked-list

--- linked-list/linked-list.cpp
+++ linked-list/linked-list.cpp
@@ -1,10 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*Nom: linked-list.cpp                                                                                                  * 
+*Auteur: Xavier Jenni                                                                                                  *
+*Date: 06.02.23                                                                                                        *
+*Description:   Initializing, creating and using the Data Structures type -> linked lists                              *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// includes & Pre-processor
 #include <iostream>
 
-#using namespace std;
+using namespace std;
 
-int main{
- 
-  cout << "Hello World!" << endl;
-  
-  return EXIT_SUCCESS;
+// Definition of an element of a linked-list
+typedef struct sElement {
+   int value;
+   sElement* pNext;
+}sElement;
+
+// Definiiton of the linked-list with 1st Element
+typedef struct sLinkedList {
+  sElement* pFirstElement;
+}sLinkedList;
+
+
+int main(){
+
+    cout << "Hello World!" << endl;
+
+    return EXIT_SUCCESS;
 }
