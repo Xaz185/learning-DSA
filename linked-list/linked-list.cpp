ac98c08bbada73ac4d3e4856bc4b57867d839a4f
Xavier <xavier.jenni@hotmail.com>
2023-03-15 21:34:24 +0100

    Creation of init function

--- linked-list/linked-list.cpp
+++ linked-list/linked-list.cpp
@@ -31,8 +31,8 @@ For the manipulation of the linked-list we need:
 */
 
 // Functions Prototypes
-void init_linked_list(void);
-void add_element(void);
+sLinkedList* init_linked_list();
+void add_element(sLinkedList* list, int newNbr);
 void remove_element(void);
 void plot_list(void);
 void delete_linked_list(void);
@@ -43,6 +43,26 @@ int main(){
     cout << "Hello World!" << endl;
 
     return EXIT_SUCCESS;
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+sLinkedList* init_linked_list(){
+
+  // allocate memory for one element of the list
+  sLinkedList* list =(sLinkedList*)malloc(sizeof(*list));
+  sElement* element =(sElement*)malloc(sizeof(*element));
+
+  // check the right allocation of memory
+  if(list == NULL || element == NULL){
+    exit(EXIT_FAILURE);
+  }
+
+  // initialisation of differents lists' element
+  element->value = 0;
+  element->pNext = NULL;  // point always to NULL if last element of the linked list
+  list->pFirstElement = element;  // point to the newest element of the linked list
+
+  return list;
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
